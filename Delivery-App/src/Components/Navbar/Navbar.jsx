385899844708fb20bd4c1f49603c35d7e41d83b7
ritikdevelop <hrithikrana216@gmail.com>
2024-07-20 21:44:18 +0530

    New commit in Front end

--- Delivery-App/src/Components/Navbar/Navbar.jsx
+++ Delivery-App/src/Components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import { StoreContext } from "../../context/StoreContext";
 
 const Navbar = ({setShowLogin}) => {
   const [menu, setMenu] = useState("home");
-  const {getTotalCartAmount} = useContext(StoreContext);
+  const {getTotalCartAmount,token,setToken} = useContext(StoreContext);
 
   return (
     <div className="navbar">
@@ -44,7 +44,19 @@ const Navbar = ({setShowLogin}) => {
           <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
           <div className={getTotalCartAmount()===0 ?"":"dot"}></div>
         </div>
-        <button onClick={() =>setShowLogin(true)}>Sign in</button>
+        {!token?<button onClick={() =>setShowLogin(true)}>Sign in</button>
+        :<div className="navbar-profile">
+          <img src={assets.profile_icon} alt="" />
+          <ul className="nav-profile-dropdown">
+            <li><img src={assets.bag_icon} alt="" />
+            <p>Orders</p>
+            </li>
+            <hr />
+            <li><img src={assets.logout_icon} alt="" />
+            <p>Logout</p></li>
+          </ul>
+        </div>
+        }
       </div>
     </div>
   );
