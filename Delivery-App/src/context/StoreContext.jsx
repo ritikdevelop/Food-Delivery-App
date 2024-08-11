0ecf43e7502853c4e577f89c861bb644b273b47a
Ritik Rana <136239988+ritikdevelop@users.noreply.github.com>
2024-08-11 16:40:57 +0530

    Update StoreContext.jsx

--- Delivery-App/src/context/StoreContext.jsx
+++ Delivery-App/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-app-pt7j.onrender.com";
 
   const [token, setToken] = useState("");
 
